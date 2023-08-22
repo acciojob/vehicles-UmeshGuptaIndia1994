83467a95e4a9ac530f37423babd1fe77b38705bf
Umesh Gupta <136713880+UmeshGuptaIndia1994@users.noreply.github.com>
2023-08-22 21:58:54 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,21 +3,17 @@
 public class Main {
     public static void main(String[] args) {
         // Example usage
-
         Car car = new Car("Toyota", 1);
         car.changeSpeed(60, 45);
-        System.out.println("Car Name: " + car.name);
-        System.out.println("Current Speed: " + car.currentSpeed);
-        System.out.println("Current Gear: " + car.currentGear);
+        
+        F1 f1 = new F1("Ferrari", 1);
+        f1.accelerate(50);
 
-        F1 f1 = new F1("Mercedes", 1);
-        f1.accelerate(120);
-        System.out.println("F1 Name: " + f1.name);
-        System.out.println("Current Speed: " + f1.currentSpeed);
-        System.out.println("Current Gear: " + f1.currentGear);
-
-        Boat boat = new Boat("Speedy", 10);
-        System.out.println("Boat Name: " + boat.getVehicleName());
-        System.out.println("Boat Capacity: " + boat.getVehicleCapacity());
+        Boat boat = new Boat("SpeedBoat", 4);
+        String boatName = boat.getVehicleName();
+        int boatCapacity = boat.getVehicleCapacity();
+        
+        System.out.println("Boat Name: " + boatName);
+        System.out.println("Boat Capacity: " + boatCapacity);
     }
 }
